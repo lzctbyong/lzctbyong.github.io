7c57bbe5e76e4c518bee64f0acb9e0e57405c0f9
liuzhengchao <190127207@qq.com>
2018-01-31 13:10:52 +0800

    zzz

--- js/index.js
+++ js/index.js
@@ -1,172 +1,175 @@
-var listKuang = $( '#list_kuang' )
-var listTool = $( '#list_tool' )
-var listStudy = $( '#list_study' )
-var pageNavKuang = $( '#pageNav_kuang' )
-var pageNavTool = $( '#pageNav_tool' )
-var pageNavStudy = $( '#pageNav_study' )
+var listKuang = $("#list_kuang")
+var listTool = $("#list_tool")
+var listStudy = $("#list_study")
+var pageNavKuang = $("#pageNav_kuang")
+var pageNavTool = $("#pageNav_tool")
+var pageNavStudy = $("#pageNav_study")
 
-$( function () {
+$(function () {
     var dataSrc
-    var urlarr = window.location.pathname.split( '/' )
-    switch ( urlarr ) {
-        case 'android.html':
-            dataSrc = 'android.json'
-        case 'study.html':
-            dataSrc = 'study.json'
-        default:
-            dataSrc = 'kuang.json'
+    var urlarr = window.location.pathname.split("/")
+    var url = urlarr[urlarr.length - 1]
+    console.log("index[11]------------", url)
+    if (url.indexOf("android") >= 0) {
+        dataSrc = "android.json"
+    } else if (url.indexOf("study") >= 0) {
+        dataSrc = "study.json"
+    } else {
+        dataSrc = "kuang.json"
     }
-    $.ajax( {
-        type       : 'GET' ,
-        contentType: 'application/json;charset=utf-8' ,
-        url        : dataSrc ,
-        dataType   : 'json' ,
-        success    : function ( res ) {
-            var kuang = res.data.kuang
-            sortUp( kuang )
-            kuang.map( ( obj , index ) => {
-                let node_item = $( '#ex_1' ).clone()
-                node_item.removeAttr( 'id' )
-                node_item.find( '.title' ).text( obj.name )
-                node_item.find( '.marks' ).text( obj.mark )
-                node_item.find( '.pngIcon' ).attr( 'src' , obj.img )
-                node_item.find( '.btn-group a' ).each( ( j , element ) => {
-                    let l = obj.link[ j ]
-                    if ( l != null ) {
-                        $( element ).attr( 'href' , l )
-                    } else {
-                        $( element ).remove()
-                    }
-                } )
-                listKuang.append( node_item )
-            } )
+    console.log("index[20]------------dataSrc", dataSrc)
 
-            for ( var i = 64 ; i < 91 ; i++ ) {
-                let node_item = $( '#ex_3' ).clone()
-                node_item.text( String.fromCharCode( i ) )
-                $( node_item ).bind( 'click' , i , function ( event ) {
-                    var arr = kuang.filter( function ( v ) {
-                        if ( event.data == 64 ) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        url: dataSrc,
+        dataType: "json",
+        success: function (res) {
+            var kuang = res.data.kuang
+            sortUp(kuang)
+            kuang.map((obj, index) => {
+                let node_item = $("#ex_1").clone()
+                node_item.removeAttr("id")
+                node_item.find(".title").text(obj.name)
+                node_item.find(".marks").text(obj.mark)
+                node_item.find(".pngIcon").attr("src", obj.img)
+                node_item.find(".btn-group a").each((j, element) => {
+                    let l = obj.link[j]
+                    if (l != null) {
+                        $(element).attr("href", l)
+                    } else {
+                        $(element).remove()
+                    }
+                })
+                listKuang.append(node_item)
+            })
+
+            for (var i = 64; i < 91; i++) {
+                let node_item = $("#ex_3").clone()
+                node_item.text(String.fromCharCode(i))
+                $(node_item).bind("click", i, function (event) {
+                    var arr = kuang.filter(function (v) {
+                        if (event.data == 64) {
                             return true
                         } else {
-                            return String.fromCharCode( event.data ) == v.name.charAt( 0 )
+                            return String.fromCharCode(event.data) == v.name.charAt(0)
                         }
-                    } )
-                    freshKuang( arr )
-                } )
-                pageNavKuang.append( node_item )
+                    })
+                    freshKuang(arr)
+                })
+                pageNavKuang.append(node_item)
             }
-            pageNavKuang.find( ':eq(1)' ).text( '*' )
+            pageNavKuang.find(":eq(1)").text("*")
 
 
             var tool = res.data.tool
-            sortUp( tool )
-            tool.map( ( obj ) => {
-                let node_item = $( '#ex_2' ).clone()
-                node_item.removeAttr( 'id' )
-                node_item.find( '.title' ).text( obj.name )
-                node_item.find( '.marks' ).text( obj.mark )
-                node_item.find( '.pngIcon' ).attr( 'src' , obj.img )
-                node_item.attr( 'href' , obj.link )
-                listTool.append( node_item )
-            } )
+            sortUp(tool)
+            tool.map((obj) => {
+                let node_item = $("#ex_2").clone()
+                node_item.removeAttr("id")
+                node_item.find(".title").text(obj.name)
+                node_item.find(".marks").text(obj.mark)
+                node_item.find(".pngIcon").attr("src", obj.img)
+                node_item.attr("href", obj.link)
+                listTool.append(node_item)
+            })
 
-            for ( var i = 64 ; i < 91 ; i++ ) {
-                let node_item = $( '#ex_4' ).clone()
-                node_item.text( String.fromCharCode( i ) )
-                $( node_item ).bind( 'click' , i , function ( event ) {
-                    var arr = tool.filter( function ( v ) {
-                        if ( event.data == 64 ) {
+            for (var i = 64; i < 91; i++) {
+                let node_item = $("#ex_4").clone()
+                node_item.text(String.fromCharCode(i))
+                $(node_item).bind("click", i, function (event) {
+                    var arr = tool.filter(function (v) {
+                        if (event.data == 64) {
                             return true
                         } else {
-                            return String.fromCharCode( event.data ) == v.name.charAt( 0 )
+                            return String.fromCharCode(event.data) == v.name.charAt(0)
                         }
-                    } )
-                    freshTool( arr )
-                } )
-                pageNavTool.append( node_item )
+                    })
+                    freshTool(arr)
+                })
+                pageNavTool.append(node_item)
             }
-            pageNavTool.find( ':eq(1)' ).text( '*' )
+            pageNavTool.find(":eq(1)").text("*")
 
             var study = res.data.study
-            sortUp( study )
-            study.map( ( obj ) => {
-                let node_item = $( '#ex_6' ).clone()
-                node_item.removeAttr( 'id' )
-                node_item.find( '.title' ).text( obj.name )
-                node_item.find( '.marks' ).text( obj.mark )
-                node_item.attr( 'href' , obj.link )
-                listStudy.append( node_item )
-            } )
+            sortUp(study)
+            study.map((obj) => {
+                let node_item = $("#ex_6").clone()
+                node_item.removeAttr("id")
+                node_item.find(".title").text(obj.name)
+                node_item.find(".marks").text(obj.mark)
+                node_item.attr("href", obj.link)
+                listStudy.append(node_item)
+            })
 
-            for ( var i = 64 ; i < 91 ; i++ ) {
-                let node_item = $( '#ex_5' ).clone()
-                node_item.text( String.fromCharCode( i ) )
-                $( node_item ).bind( 'click' , i , function ( event ) {
-                    var arr = study.filter( function ( v ) {
-                        if ( event.data == 64 ) {
+            for (var i = 64; i < 91; i++) {
+                let node_item = $("#ex_5").clone()
+                node_item.text(String.fromCharCode(i))
+                $(node_item).bind("click", i, function (event) {
+                    var arr = study.filter(function (v) {
+                        if (event.data == 64) {
                             return true
                         } else {
-                            return String.fromCharCode( event.data ) == v.name.charAt( 0 )
+                            return String.fromCharCode(event.data) == v.name.charAt(0)
                         }
-                    } )
-                    freshStudy( arr )
-                } )
-                pageNavStudy.append( node_item )
+                    })
+                    freshStudy(arr)
+                })
+                pageNavStudy.append(node_item)
             }
-            pageNavStudy.find( ':eq(1)' ).text( '*' )
+            pageNavStudy.find(":eq(1)").text("*")
         }
-    } )
+    })
 
-    function sortUp( list ) {
-        list.sort( function ( v1 , v2 ) {
-            return v1.name.charCodeAt( 0 ) - v2.name.charCodeAt( 0 )
-        } )
+    function sortUp(list) {
+        list.sort(function (v1, v2) {
+            return v1.name.charCodeAt(0) - v2.name.charCodeAt(0)
+        })
     }
 
-    function freshKuang( v ) {
-        listKuang.find( '#ex_1' ).siblings().remove()
-        v.map( ( obj , index ) => {
-            let node_item = $( '#ex_1' ).clone()
-            node_item.removeAttr( 'id' )
-            node_item.find( '.title' ).text( obj.name )
-            node_item.find( '.marks' ).text( obj.mark )
-            node_item.find( '.pngIcon' ).attr( 'src' , obj.img )
-            node_item.find( '.btn-group a' ).each( ( j , element ) => {
-                let l = obj.link[ j ]
-                if ( l != null ) {
-                    $( element ).attr( 'href' , l )
+    function freshKuang(v) {
+        listKuang.find("#ex_1").siblings().remove()
+        v.map((obj, index) => {
+            let node_item = $("#ex_1").clone()
+            node_item.removeAttr("id")
+            node_item.find(".title").text(obj.name)
+            node_item.find(".marks").text(obj.mark)
+            node_item.find(".pngIcon").attr("src", obj.img)
+            node_item.find(".btn-group a").each((j, element) => {
+                let l = obj.link[j]
+                if (l != null) {
+                    $(element).attr("href", l)
                 } else {
-                    $( element ).remove()
+                    $(element).remove()
                 }
-            } )
-            listKuang.append( node_item )
-        } )
+            })
+            listKuang.append(node_item)
+        })
     }
 
-    function freshTool( v ) {
-        listTool.find( '#ex_2' ).siblings().remove()
-        v.map( ( obj , index ) => {
-            let node_item = $( '#ex_2' ).clone()
-            node_item.removeAttr( 'id' )
-            node_item.find( '.title' ).text( obj.name )
-            node_item.find( '.marks' ).text( obj.mark )
-            node_item.find( '.pngIcon' ).attr( 'src' , obj.img )
-            node_item.attr( 'href' , obj.link )
-            listTool.append( node_item )
-        } )
+    function freshTool(v) {
+        listTool.find("#ex_2").siblings().remove()
+        v.map((obj, index) => {
+            let node_item = $("#ex_2").clone()
+            node_item.removeAttr("id")
+            node_item.find(".title").text(obj.name)
+            node_item.find(".marks").text(obj.mark)
+            node_item.find(".pngIcon").attr("src", obj.img)
+            node_item.attr("href", obj.link)
+            listTool.append(node_item)
+        })
     }
 
-    function freshStudy( v ) {
-        listStudy.find( '#ex_6' ).siblings().remove()
-        v.map( ( obj , index ) => {
-            let node_item = $( '#ex_6' ).clone()
-            node_item.removeAttr( 'id' )
-            node_item.find( '.title' ).text( obj.name )
-            node_item.find( '.marks' ).text( obj.mark )
-            node_item.find( '.pngIcon' ).attr( 'src' , obj.img )
-            node_item.attr( 'href' , obj.link )
-            listStudy.append( node_item )
-        } )
+    function freshStudy(v) {
+        listStudy.find("#ex_6").siblings().remove()
+        v.map((obj, index) => {
+            let node_item = $("#ex_6").clone()
+            node_item.removeAttr("id")
+            node_item.find(".title").text(obj.name)
+            node_item.find(".marks").text(obj.mark)
+            node_item.find(".pngIcon").attr("src", obj.img)
+            node_item.attr("href", obj.link)
+            listStudy.append(node_item)
+        })
     }
-} )
+})
